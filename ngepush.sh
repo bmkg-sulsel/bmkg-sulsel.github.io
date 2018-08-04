f44717bbd731a29ca3cde3309a60b635660c956b
yosiknorman <yosikbrebes@gmail.com>
2018-08-04 21:06:47 +0800

    BISMILLAH

---
+++ ngepush.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r ../BMKG-SULSEL/* .
+git status
+git add .
+git commit -m "BISMILLAH"
+git push -f --repo https://bmkg-sulsel:brambang_07@github.com/bmkg-sulsel/bmkg-sulsel.github.io.git
