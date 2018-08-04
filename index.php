9ed86b887e9309b33c5767ec194e7935c5cd31dc
yosiknorman <yosikbrebes@gmail.com>
2018-08-04 20:21:10 +0800

    BISMILLAH

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+
+ 	
+ </head>
+ <body>
+ 	<?php
+		for ($x = 0; $x <= 10; $x++) {
+    		echo "The number is: $x <br>";
+		}
+	?> 
+ 
+ </body>
+ </html> 
---
+++ nyerver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while [[ true ]]; do
+	echo "`php index.php`" > menjajal.html
+	sleep 10
+done
+
